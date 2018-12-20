88a04d97db9d09f910825de806c1c9a146c8598a
krassowska <39529572+krassowska@users.noreply.github.com>
2018-12-20 23:17:53 +0000

    Restore script.js lost in merge 2ef9996

---
+++ thirdparty/js/script.js
@@ -0,0 +1,111 @@
+//START
+/* https://bootsnipp.com/snippets/featured/simple-vertical-tab */
+
+$(document).ready(function() {
+    $("div.bhoechie-tab-menu>div.list-group>a").click(function(e) {
+        e.preventDefault();
+        $(this).siblings('a.active').removeClass("active");
+        $(this).addClass("active");
+        var index = $(this).index();
+        $("div.bhoechie-tab>div.bhoechie-tab-content").removeClass("active");
+        $("div.bhoechie-tab>div.bhoechie-tab-content").eq(index).addClass("active");
+    });
+});
+
+//End
+
+
+//START
+
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+//END
+
+// --------------------------------------------------------------
+
+// START
+// https://codepen.io/jaycbrf/pen/iBszr
+$(document).ready(function() {
+	$('#contact_form').bootstrapValidator({
+			// To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+			feedbackIcons: {
+					valid: 'glyphicon glyphicon-ok',
+					invalid: 'glyphicon glyphicon-remove',
+					validating: 'glyphicon glyphicon-refresh'
+			},
+			fields: {
+					name: {
+							validators: {
+											stringLength: {
+											min: 2,
+									},
+											notEmpty: {
+											message: 'Please supply your name'
+									}
+							}
+					},
+					email: {
+							validators: {
+									notEmpty: {
+											message: 'Please supply your email address'
+									},
+									emailAddress: {
+											message: 'Please supply a valid email address'
+									}
+							}
+					},
+					comment: {
+							validators: {
+										stringLength: {
+											min: 10,
+											max: 500,
+											message:'Please enter at least 10 characters and no more than 500'
+									},
+									notEmpty: {
+											message: 'Please supply your message'
+									}
+									}
+							}
+					}
+			})
+			.on('success.form.bv', function(e) {
+					$('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+							$('#contact_form').data('bootstrapValidator').resetForm();
+
+					// Prevent form submission
+					e.preventDefault();
+
+					// Get the form instance
+					var $form = $(e.target);
+
+					// Get the BootstrapValidator instance
+					var bv = $form.data('bootstrapValidator');
+
+					// Use Ajax to submit form data
+					$.post($form.attr('action'), $form.serialize(), function(result) {
+							console.log(result);
+					}, 'json');
+			});
+});
+
+//END
+// --------------------------------------------------------------
